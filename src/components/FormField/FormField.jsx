88f3b03f83cac72427c4fb5e8e74a9e9071a3695
cyn-rus <13519118@std.stei.itb.ac.id>
2021-05-12 03:27:41 +0700

    fix form field

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -1,11 +1,22 @@
 import React from "react"
+
 import "./FormField.css"
 
-function FormField({name, label, ...rest}) {
+
+function FormField({name, label, touched, errors, ...rest}) {
     return (
         <div className="form-field">
             <label htmlFor={name}>{label}</label>
-            <input id={name} name={name} {...rest}/>
+            <div className="input-field">
+                <div style={{color: touched && errors ? '' : "#0i3f59"}} className="error-message">
+                    {touched && errors ? errors : '.'}
+                </div>
+                <input 
+                    id={name} 
+                    name={name} 
+                    {...rest}
+                />
+            </div>
         </div>
     )
 }
