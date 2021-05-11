9b0435d508feb41b29360bff0a2fe1bcf2f5d548
cyn-rus <13519118@std.stei.itb.ac.id>
2021-05-12 03:26:53 +0700

    finished dob page

---
+++ src/pages/DOB/DOB.jsx
@@ -0,0 +1,53 @@
+import React, { useContext } from "react"
+import { useHistory } from "react-router-dom"
+import DatePicker from "react-datepicker"
+import Template from '../../components/Template/Template'
+import Button from "../../components/Button/Button"
+import "react-datepicker/dist/react-datepicker.css";
+
+import { AppContext } from '../../context';
+
+import "./DOB.css"
+
+const DOB = () => {
+    const history = useHistory()
+    const  { formData, setFormData } = useContext(AppContext)
+
+    return (
+        <Template>
+            <div className="dob-page">
+                <Button
+                    secondary
+                    type="submit"
+                    onClick={() => history.push("/personal")}
+                >
+                    ← Personal
+                </Button>
+                <div className="dob-content">
+                    <h1>Date of Birth</h1>
+                    <DatePicker
+                        className="date-picker"
+                        selected={new Date()}
+                        onChange={newDate => {
+                                const newFormValue = {...formData, dob: newDate} 
+                                localStorage.setItem('formData', JSON.stringify(newFormValue))
+                                setFormData(newFormValue)
+                            } 
+                        }
+                    />
+                </div>
+                <div className="next-page-button">
+                    <Button
+                        primary
+                        type="submit"
+                        onClick={() => history.push("/agreement")}
+                    >
+                        NEXT PAGE
+                    </Button>
+                </div>
+            </div>
+        </Template>
+    )
+}
+
+export default DOB
